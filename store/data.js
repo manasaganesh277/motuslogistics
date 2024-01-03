985df5468410574a150cd0bb3feed9804079f730
manasa-277 <73336679+manasa-277@users.noreply.github.com>
2024-01-03 17:48:27 +0530

    new

---
+++ store/data.js
@@ -0,0 +1,38 @@
+// A collection of musicians where the key is the unique id
+module.exports = {
+  coltrane: {
+    firstName: 'John',
+    lastName: 'Coltrane',
+    large: 'large',
+  },
+
+  davis: {
+    firstName: 'Gustav',
+    lastName: 'Ejstes',
+    large: ' large',
+  },
+
+  mccartney: {
+    firstName: 'Paul',
+    lastName: 'McCartney',
+    large: 'large',
+  },
+
+  hendrix: {
+    firstName: 'Jimi',
+    lastName: 'Hendrix',
+    large: ' large',
+  },
+
+  cobain: {
+    firstName: 'Kurt',
+    lastName: 'Cobain',
+    large: ' large',
+  },
+
+  king: {
+    firstName: 'Bo',
+    lastName: 'Hansson',
+    large: ' large',
+  },
+};
